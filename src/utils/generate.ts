9386792190eb15a34e4a7e7cb12d84616a363864
wayne <wei.zheng02@weimob.com>
2022-01-12 10:12:33 +0800

    feat(增加格式校验): feat: 增加校验

--- src/utils/generate.ts
+++ src/utils/generate.ts
@@ -4,16 +4,18 @@ import { log } from "./log";
 export const generate = async (options: {
   input: string
   output: string
-  install?: boolean
 }) => {
   try {
-    const { input, output, install = false } = options
+    const { input, output } = options
     await $`cp -r ${input} ${output}`
     await cd(output)
-    if (install) {
-      await $`pnpm install`
-      await $`pnpm changeset init`
-    }
+    await $`pnpm install`
+    await Promise.all([
+      $`pnpm changeset init`,
+      $`pnpm prepare`,
+      $`pnpm husky add .husky/pre-commit "echo \"代码格式检查中...\"\nlint-staged"`,
+      $`pnpm husky add .husky/commit-msg "echo \"commit msg 格式校验中...\"\nnpx --no-install commitlint --edit $1"`,
+    ])
     log('模板生成成功')
   } catch (e) {
     console.error(e)
